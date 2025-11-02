0e1cfd6373e70723b13c41f97435e86ae17d9908
Cody <65142939+Coboll@users.noreply.github.com>
2025-11-01 18:55:54 -0700

    Update levels-main.js

--- level/levels-main.js
+++ level/levels-main.js
@@ -6,7 +6,7 @@ const d = {
             {
                   "user": "Andrew14916",
                   "link": "https://www.youtube.com/watch?v=pi0lRGRfZDQ",
-                  "percent": 99,
+                  "percent": 100,
                   "hz": "240hz"
               },
             
