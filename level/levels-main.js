119fb255ba8e1081285802d7a8674f644018a884
Cody <65142939+Coboll@users.noreply.github.com>
2025-12-07 00:46:00 -0800

    Update levels-main.js

--- level/levels-main.js
+++ level/levels-main.js
@@ -536,6 +536,12 @@ const d = {
     {
 
           "vids": [
+             {
+                  "user": "Coboll",
+                  "link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1",
+                  "percent": 100,
+                  "hz": "240hz"
+              },
             
           ],
           "name": "Shotty Exens Limit",
